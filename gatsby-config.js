a0c26b517b875352acdb36dfb7a72c6059f67dbc
marcocossali <marco.cossali@freeloop.it>
2024-02-08 16:48:23 +0100

    Removed unused package

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,14 +16,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: `${__dirname}/src/content/images/svg/`,
-        },
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `content`,
