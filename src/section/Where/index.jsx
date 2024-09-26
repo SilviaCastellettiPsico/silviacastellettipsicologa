8ebd17cd00b096fd301c4e750448a92f6193ff52
marcocossali <marco.cossali@freeloop.it>
2024-09-26 15:52:19 +0200

    Modified pages style

--- src/section/Where/index.jsx
+++ src/section/Where/index.jsx
@@ -11,18 +11,45 @@ export function Where({ sectionId }) {
   const { html } = data.markdownRemark;
 
   return (
-    <section>
-      <div id={sectionId} className="section-container">
-        {isSmallScreen ? (
-          <>
-            <div className="section-title">{where.title}</div>
+    <section id={sectionId} className="section-container">
+      {isSmallScreen ? (
+        <>
+          <div className="section-title">{where.title}</div>
+          <div className="where col-sm-12 col-md-6 col-lg-6">
+            <div
+              className="section-description"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+            <button
+              style={{ marginRight: "auto", marginBottom: "20px" }}
+              type="button"
+              className="btn btn-primary"
+              onClick={() => window.open(where.ctaButton.url, "__blank")}
+            >
+              {where.ctaButton.label}
+            </button>
+          </div>
+          <GatsbyImage
+            className="section-image col-sm-12 col-md-6 col-lg-6"
+            imgClassName="section-image"
+            image={getImage(where.image2.src)}
+            alt={where.image2.alt}
+            loading="eager"
+          />
+        </>
+      ) : (
+        <div className="container">
+          <div className="row">
             <div className="where col-sm-12 col-md-6 col-lg-6">
+              <div className="section-title">{where.title}</div>
               <div
                 className="section-description"
                 dangerouslySetInnerHTML={{ __html: html }}
               />
               <button
-                style={{ marginRight: "auto", marginBottom: "20px" }}
+                style={{
+                  marginRight: "auto",
+                }}
                 type="button"
                 className="btn btn-primary"
                 onClick={() => window.open(where.ctaButton.url, "__blank")}
@@ -37,38 +64,9 @@ export function Where({ sectionId }) {
               alt={where.image2.alt}
               loading="eager"
             />
-          </>
-        ) : (
-          <div className="container">
-            <div className="row">
-              <div className="where col-sm-12 col-md-6 col-lg-6">
-                <div className="section-title">{where.title}</div>
-                <div
-                  className="section-description"
-                  dangerouslySetInnerHTML={{ __html: html }}
-                />
-                <button
-                  style={{
-                    marginRight: "auto",
-                  }}
-                  type="button"
-                  className="btn btn-primary"
-                  onClick={() => window.open(where.ctaButton.url, "__blank")}
-                >
-                  {where.ctaButton.label}
-                </button>
-              </div>
-              <GatsbyImage
-                className="section-image col-sm-12 col-md-6 col-lg-6"
-                imgClassName="section-image"
-                image={getImage(where.image2.src)}
-                alt={where.image2.alt}
-                loading="eager"
-              />
-            </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </section>
   );
 }
